27a63ea9ec3ebcf59fa674c5d1ed64adb9bbfdd6
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-10 18:40:37 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Configuração da base do caminho, se aplicável
-    basePath: '/meu-app',
-  
-    // Manipulação de cabeçalhos para URLs absolutas
-    async headers() {
-      return [
-        {
-          source: '/',
-          headers: [
-            {
-              key: 'X-Forwarded-Host',
-              value: 'seu-host.com',
-            },
-          ],
-        },
-      ];
-    },
-  
-    // Manipulação de redirecionamentos para URLs absolutas
-    async redirects() {
-      return [
-        {
-          source: '/old-path',
-          destination: '/new-path',
-          permanent: true,
-        },
-      ];
-    },
-  
-    // Confie em cabeçalhos de proxy confiáveis
-    serverRuntimeConfig: {
-      trustProxy: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  // Configuração da base do caminho, se aplicável
+  basePath: '',
+
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'X-Forwarded-Host',
+            value: '15.228.38.79',
+          },
+        ],
+      },
+    ];
+  },
+
+  async redirects() {
+    return [];
+  },
+
+  serverRuntimeConfig: {
+    trustProxy: true,
+  },
+};
+
+export default nextConfig;
