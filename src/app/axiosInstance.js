551604617bd792df4bd579a903ce3c4f0e4de0b2
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-13 14:38:39 -0300

    .

--- src/app/axiosInstance.js
+++ src/app/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://15.228.38.79:4000',
+    baseURL: 'https://15.228.38.79:4000',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
