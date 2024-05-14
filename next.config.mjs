b9109a0c4d0d18ad1b81133a586c44756500d21f
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-05-14 14:45:59 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// Configurações do servidor Next.js
-export async function headers() {
-    return [
-        {
-            source: '/',
-            headers: [
-                {
-                    key: 'Access-Control-Allow-Origin',
-                    value: '*', // Altere para o domínio desejado se necessário
-                },
-            ],
-        },
-    ];
-}
-
-// Configurações do servidor Next.js
-export const server = {
-    host: '15.228.38.79', // Substitua pelo IP do seu servidor
-    port: 29, // Porta que deseja utilizar
-};
+export default nextConfig;
