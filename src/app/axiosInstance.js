446b5bd3ec209c0cfc200040fb847ab333ed2a98
Nicollas Davi de Souza Brandão <132391248+NicollasDavi@users.noreply.github.com>
2024-07-02 13:35:54 -0300

    .

--- src/app/axiosInstance.js
+++ src/app/axiosInstance.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://15.228.38.79:29/api',
+    baseURL: 'https://15.228.38.79:29/api',  // Certifique-se de usar HTTPS e a porta correta
     timeout: 15000,
     headers: {
         'Content-Type': 'application/json',
-    }
+    },
+    withCredentials: true  // Inclui cookies e outros credenciais nas requisições
 });
 
 instance.interceptors.request.use(async config => {
